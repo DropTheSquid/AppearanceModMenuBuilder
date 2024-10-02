8c75b80b477072b8ff186ac19e7d8325017f94d5
DropTheSquid <DropTheSquid17@gmail.com>
2024-10-02 16:26:19 -0400

    moved conditionals into startup file so I can reference stuff from there.

--- AppearanceModMenuBuilder/LE1/BuildSteps/DLC/BuildPlotManagerFile.cs
+++ AppearanceModMenuBuilder/LE1/BuildSteps/DLC/BuildPlotManagerFile.cs
@@ -9,16 +9,11 @@ namespace AppearanceModMenuBuilder.LE1.BuildSteps.DLC
     {
         public void RunModTask(ModBuilderContext context)
         {
-            Console.WriteLine("Building PlotManagerDLC_AMM.pcc");
-            
-            var plotPackageFile = MEPackageHandler.CreateAndOpenPackage(Path.Combine(context.CookedPCConsoleFolder, "PlotManagerDLC_AMM.pcc"), context.Game);
-
-            // make an object referencer (probably not strictly necessary? LE1 can dynamic load without this)
-            plotPackageFile.GetOrCreateObjectReferencer();
+            Console.WriteLine("Building PlotManager into startup");
 
             // add a few classes
             var classTask = new AddClassesToFile(
-                _ => plotPackageFile,
+                _ => context.GetStartupFile()!,
                 // the handler inheritance tree
                 LooseClassCompile.GetClassFromFile(@"Resources\LE1\Shared\BioAutoConditionals.uc", []));
             classTask.RunModTask(context);
